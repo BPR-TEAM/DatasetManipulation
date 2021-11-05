efffb8184b58b17ff1f155f11030e86f73c9923e
Nitramzz <navaron2@gmail.com>
2021-11-05 14:35:08 +0100

    testing

---
+++ Image.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace DatasetManipulation
+{
+    class Image
+    {
+        private string path{get;set;}
+        private int quality{get;set;}
+
+        public void rotate(int degrees)
+        {
+            
+        }
+
+        public void flip()
+        {
+
+        }
+
+        public void mirror()
+        {
+
+        }
+
+        public void adjustQualityLevel()
+        {
+
+        }
+    }
+}
