73e3204434d69f72ee72d8952bcae9e5758c9852
Nitramzz <navaron2@gmail.com>
2021-11-05 14:14:45 +0100

    Init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DatasetManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
