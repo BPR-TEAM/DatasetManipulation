465a491c9e890ff73224e445f40636ded03590fb
Nitramzz <navaron2@gmail.com>
2021-12-09 12:48:50 +0100

    Now deletes any folder that has under 5 images

--- Program.cs
+++ Program.cs
@@ -6,17 +6,24 @@ namespace DatasetManipulation
     {
         static void Main(string[] args)
         {
-            string path = @"path_to_image_directory_goes_here";
+            string path = @"F:\Downloads\Machine Learning personal dataset\Machine Learning personal dataset";
             DirectoryInfo[] directories = new DirectoryInfo(path).GetDirectories();
             foreach(DirectoryInfo dir in directories)
             {
-                Images images = new Images();
-                images.path = dir.FullName + @"\";
-                images.rotate();
-                images.flip();
-                images.mirror();
-                images.adjustQualityLevel(60L);
-                images.adjustQualityLevel(20L);
+                if(dir.GetFiles().Length >= 5)
+                {
+                    Images images = new Images();
+                    images.path = dir.FullName + @"\";
+                    images.rotate();
+                    images.flip();
+                    images.mirror();
+                    images.adjustQualityLevel(60L);
+                    images.adjustQualityLevel(20L);
+                }
+                else
+                {
+                    dir.Delete(true);
+                }
             }
         }
     }
